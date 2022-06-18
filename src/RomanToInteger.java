982045c11dcb73b68779903bd5da4ab799823238
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-17 19:46:02 -0700

    created a method to convert the roman values to integers.

--- src/RomanToInteger.java
+++ src/RomanToInteger.java
@@ -4,6 +4,7 @@ import java.util.Map;
 public class RomanToInteger {
     public static int romanToInteger(String s){
         Map<Character,Integer> map = new HashMap<>();
+        //get the keys as the roman values and thier correspond values as objects
         map.put('I',1);
         map.put('V',5);
         map.put('X',10);
@@ -12,15 +13,19 @@ public class RomanToInteger {
         map.put('D',500);
         map.put('M',1000);
 
+        //initialize the result with the last(index-wise) value of the string
         int result = map.get(s.charAt(s.length()-1));
+
+        //running a loop thu string from the second last element to the first one
         for(int i=s.length()-2; i>=0; i--){
+            //where-ever it finds a value less than the consecutive next char, it subtracts it from result
             if(map.get(s.charAt(i))<map.get(s.charAt(i+1))){
                 result = result-map.get(s.charAt(i));
-            }
+            }//else it adds
             else{
                 result = result+ map.get(s.charAt(i));
             }
-
+        //hence return the result
     }return result;
 }
     public static void main(String[] args) {
